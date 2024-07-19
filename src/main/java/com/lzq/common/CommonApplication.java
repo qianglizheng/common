a99a10fc6a7c0bd587003b75658c65af940f77af
lzq <1648785@qq.com>
2024-07-19 16:29:44 +0800

    初始化项目

--- src/main/java/com/lzq/common/CommonApplication.java
+++ src/main/java/com/lzq/common/CommonApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommonApplication {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(CommonApplication.class, args);
     }
-
 }
