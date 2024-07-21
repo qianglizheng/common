6564c876e4997c135fb1bf1cfd9af33894b5696a
qianglizheng <1648785@qq.com>
2024-07-21 20:06:07 +0800

    冗余去除

--- src/main/java/com/lzq/common/CommonApplication.java
+++ src/main/java/com/lzq/common/CommonApplication.java
@@ -3,8 +3,6 @@ package com.lzq.common;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootApplication
 @MapperScan("com.lzq.common.mapper")
--- src/test/java/com/lzq/common/CommonApplicationTests.java
+++ src/test/java/com/lzq/common/CommonApplicationTests.java
@@ -1,28 +1,12 @@
 package com.lzq.common;
 
-import com.lzq.common.mapper.SysUserMapper;
-import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class CommonApplicationTests {
-    @Resource
-    SysUserMapper sysUserMapper;
-
-    @Resource
-    StringRedisTemplate stringRedisTemplate;
-
-    @Resource
-    RedisTemplate<String,Object> redisTemplate;
-
     @Test
     void contextLoads() {
-        stringRedisTemplate.opsForValue();
-        System.out.println(redisTemplate.opsForValue().get("name1"));
-    }
 
+    }
 }
