bb9a176db61e4ddb7e839b594dd04a312270e821
qianglizheng <1648785@qq.com>
2024-08-04 15:48:08 +0800

    登录拦截器

---
+++ src/main/java/com/lzq/common/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.lzq.common.config;
+
+import com.lzq.common.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/**");
+    }
+}
---
+++ src/main/java/com/lzq/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,20 @@
+package com.lzq.common.interceptor;
+
+import com.lzq.common.util.UserHolderUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // TODO 获取用户信息
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolderUtil.removeUser();
+    }
+}
